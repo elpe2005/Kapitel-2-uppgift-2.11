2735fb7d27bdd67909afd91d1663f7a3567e613c
elpe2005 <111962532+elpe2005@users.noreply.github.com>
2022-09-09 11:04:43 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Uppgift_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hur långt hoppade Elin och Alma i meter? ");
+            string längd = Console.ReadLine();
+            Console.WriteLine(" Alma hoppade 3.56 meter ");
+            string längden =Console.ReadLine();
+
+            Console.WriteLine( längd + längden + " Men Elin hoppade 1 meter längre ");
+        }
+    }
+}
